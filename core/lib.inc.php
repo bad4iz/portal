84a2aa66932f8cbea1aee4cf643e2fe5b1d4ddde
bad4iz <bad4iz@gmail.com>
2017-10-13 03:15:17 +0400

    автозагрузка классов

--- core/lib.inc.php
+++ core/lib.inc.php
@@ -1,7 +1,28 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: bad4iz
- * Date: 13.10.17
- * Time: 2:43
+ * библиотека функций
  */
+
+// автозагрузка классов
+function mAutoLoad($className) {
+  $className = str_replace('\\', '/', $className);
+  $ind = strpos($className, '/');
+  if ($ind) {
+    $package = strtolower(substr($className, 0,$ind)); // получаем имя пакета в нижнем регистре
+    $className = substr($className, $ind + 1); // получаем путь
+  }
+  switch ($package){
+    case 'core':
+      requireFile('../core/' . $className . '.php');
+      break;
+    default:
+      requireFile('/project/' . $package . '/'  . $className . '.php');
+  }
+}
+
+// подключение файла
+function requireFile($path) {
+  if (file_exists($path)) {
+    include $path;
+  }
+}
