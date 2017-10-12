aa177009b2bc8753c831f465da1a27648b7d6afd
bad4iz <bad4iz@gmail.com>
2017-10-13 03:00:55 +0400

    добавление файла для начальной загрузки

---
+++ core/bootsrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * файл начальной загрузки
+ */
+
+// подключение библиотеки
+require_once './lib.inc.php';
+
+// регистрация автолоудера
+spl_autoload_register('mAutoLoad');
+// регистрация обработчика ошибок
+set_error_handler('mError');
+
+// подключение вендора
+require_once '../vendor/autoload.php';
